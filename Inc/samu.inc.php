20f38f1a9ab533d032d7ca40c12fe0e1ed05c6c5
omoluabidotcom <yyahaya222@yahoo.com>
2021-04-25 13:40:20 +0100

    An addition was made due to breakage in former code

--- Inc/samu.inc.php
+++ Inc/samu.inc.php
@@ -1,5 +1,12 @@
 <?php
 
+include 'autoload.inc.php';
 
+$first_num = $_POST['first_num'];
+$second_num = $_POST['second_num'];
+$operator = $_POST['symbol'];
+
+$calculation = new Calc($first_num, $second_num, $operator);
+echo $calculation->calculate();
 
 ?>
